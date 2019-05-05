77f6139137a26e2d1fe536897a8aedfcdb34b57d
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-05-05 22:32:15 +0200

    Added tests

--- test/unit/snapshy_test.exs
+++ test/unit/snapshy_test.exs
@@ -1,10 +1,26 @@
+defmodule Struct do
+  defstruct key: "value"
+end
+
 defmodule SnapshyTest do
   use Snapshy
   use ExUnit.Case
 
   doctest Snapshy
 
-  test_snapshot "a returns a double of a given number" do
-    ["abcd", "efgh", "hello"]
+  test "it saves a file for long keyword lists" do
+    match_snapshot(Enum.map(1..100, fn x -> {:hello, x} end))
+  end
+
+  test "it saves a file for long binaries" do
+    match_snapshot(Enum.join(1..10000, ","))
+  end
+
+  test "it saves a file for custom structs with default data" do
+    match_snapshot(%Struct{})
+  end
+
+  test "it saves a file for custom structs" do
+    match_snapshot(%Struct{key: "different_value"})
   end
 end
