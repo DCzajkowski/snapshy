9adbf61fd625fdcc65ea609cabb5662f29ddcb24
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-05-27 17:23:41 +0200

    Bumped the version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Snapshy.MixProject do
   def project do
     [
       app: :snapshy,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
